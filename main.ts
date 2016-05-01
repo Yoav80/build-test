53e2faa53bce836ab04b384699b14a3822f25262
yoav melkman <y_milkman@hotmail.com>
2016-05-01 13:54:53 +0300

    first upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+/// <reference path="typings/globals.d.ts" />
+
+import "angular";
+import {module} from "./app/common/module";
+import "./app/directives/componentName";
+import "./app/components/app";
+
+init();
+
+function init() {
+    var element = document.getElementById("html");
+    if(!element) {
+        console.error("Root element was not found");
+        return;
+    }
+    
+    angular.bootstrap(element, [module.name]);
+}
